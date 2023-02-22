5749839068ed68333bd6cfbb6c1b3bf84c73d7cb
hkthomassen <h.thomassen@outlook.de>
2023-02-22 21:33:10 +0100

    Displayed forecast data using the API

--- src/app.js
+++ src/app.js
@@ -21,27 +21,44 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
+  let forecast = response.data.daily;
+  console.log(forecast);
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
               <div class="col-2">
-                <div class="weather-forecast-days">${day}</div>
+                <div class="weather-forecast-days">${formatDay(
+                  forecastDay.time
+                )}</div>
                 <img
-                  src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
+                  src="${forecastDay.condition.icon_url}"
                   alt=""
                   width="36px"
                 />
                 <div class="weather-forecast-temperatures">
-                  <span class="weather-forecast-temperatures-max">18°</span>
-                  <span class="weather-forecast-temperatures-min">12°</span>
+                  <span class="weather-forecast-temperatures-max">${Math.round(
+                    forecastDay.temperature.maximum
+                  )}°</span>
+                  <span class="weather-forecast-temperatures-min">${Math.round(
+                    forecastDay.temperature.minimum
+                  )}°</span>
                 </div>
               </div>
     `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
